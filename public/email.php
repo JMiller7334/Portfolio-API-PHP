4765c19410da5c2ea90d7655cfd0a9a264a562a6
JMiller7334 <Jacob-Miller@Hotmail.com>
2025-06-01 14:41:38 -0500

    updated to production version.

--- public/email.php
+++ public/email.php
@@ -1,4 +1,16 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
+
+// Handle preflight requests
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
